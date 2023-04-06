5adfbab89d809bd9207d7867c5108e2952a42fce
drobyshev.a <monlil7@mail.ru>
2023-04-06 09:08:49 +0700

    1123

---
+++ src/Lessons28/Package1/Person.java
@@ -0,0 +1,8 @@
+package Lessons28.Package1;
+
+public class Person {
+    protected String name;
+    public Person(){
+        name = "Kuk";
+    }
+}
---
+++ src/Lessons28/Package1/Test.java
@@ -0,0 +1,8 @@
+package Lessons28.Package1;
+
+public class Test {
+    public static void main(String[] args) {
+        Person p1 = new Person();
+        System.out.println(p1.name);
+    }
+}
